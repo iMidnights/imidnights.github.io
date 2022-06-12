88ea9e0c6568f1e972b60df9a1a872e9b5b86433
Mohamed <mohaidarus@gail.com>
2022-06-12 18:13:43 +0100

    updatee

--- Oval-Knight/assets/script.js
+++ Oval-Knight/assets/script.js
@@ -1,68 +1,9 @@
-// function LoadData() {
-//   const http = new XMLHttpRequest();
-//   const url = 'https://236f935f-8f97-4113-a394-ae28e91900a2.id.repl.co/TopTen';
+function fscreen() {
+    var element = document.getElementById("embededGame");
 
-//   http.open("GET", url);
-//   http.send();
-
-//   http.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//       const rawJson = JSON.parse(http.responseText);
-
-//       let inner = "";
-
-
-//       for (var i = 0; i < rawJson['L'].length; i++) {
-//         var object = rawJson['L'][i];
-
-//         var name = object['name'];
-//         var kills = object['e'];
-
-//         inner += `<div class="panel-content" id="item10">
-//         <div class="item-card d-flex">
-//           <div class="cross">
-//             <h1>${i + 1}</h1>
-//           </div>
-//             <p style="padding-right: 0; width: 60%;">${name}</p>
-//             <div role="progressbars" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="--value:40; --level: 10"></div>
-//         </div>
-//       </div>`
-//       }
-
-//       document.getElementById("top-10").innerHTML = inner;
-//     }
-//   }
-// }
-
-// function LoadEKills() {
-//   const http = new XMLHttpRequest();
-//   const url = 'https://236f935f-8f97-4113-a394-ae28e91900a2.id.repl.co/TopE';
-
-//   http.open("GET", url);
-//   http.send();
-//   http.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//       var rawJson = JSON.parse(http.responseText);
-//       var inner = "";
-
-//       for (var i = 0; i < rawJson['L'].length; i++) {
-//         var object = rawJson['L'][i];
-
-//         var name = object['name'];
-//         var kills = object['e'];
-
-//         inner += `<div class="panel-content" id="item10">
-//         <div class="item-card d-flex">
-//           <div class="cross">
-//             <h1>${i + 1}</h1>
-//           </div>
-//             <p style="padding-right: 0; width: 60%;">${name}</p>
-//             <div role="progressbars" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="--value:40; --level: 10"></div>
-//         </div>
-//       </div>`
-//       }
-
-//       document.getElementById("enemy-kills").innerHTML = inner;
-//     }
-//   }
-// }
+    if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    }
+}
