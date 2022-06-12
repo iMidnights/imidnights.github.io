69c17d5e9591c82dc04419b5588296e2d5d85adf
Muridi <m.muridi10022004@gmail.com>
2022-06-12 16:04:02 +0100

    Update script.js

--- Oval-Knight/assets/script.js
+++ Oval-Knight/assets/script.js
@@ -1,68 +1,68 @@
-// function LoadData() {
-//   const http = new XMLHttpRequest();
-//   const url = 'https://236f935f-8f97-4113-a394-ae28e91900a2.id.repl.co/TopTen';
+function LoadData() {
+  const http = new XMLHttpRequest();
+  const url = 'https://236f935f-8f97-4113-a394-ae28e91900a2.id.repl.co/TopTen';
 
-//   http.open("GET", url);
-//   http.send();
+  http.open("GET", url);
+  http.send();
 
-//   http.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//       const rawJson = JSON.parse(http.responseText);
+  http.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      const rawJson = JSON.parse(http.responseText);
 
-//       let inner = "";
+      let inner = "";
 
 
-//       for (var i = 0; i < rawJson['L'].length; i++) {
-//         var object = rawJson['L'][i];
+      for (var i = 0; i < rawJson['L'].length; i++) {
+        var object = rawJson['L'][i];
 
-//         var name = object['name'];
-//         var kills = object['e'];
+        var name = object['name'];
+        var kills = object['e'];
 
-//         inner += `<div class="panel-content" id="item10">
-//         <div class="item-card d-flex">
-//           <div class="cross">
-//             <h1>${i + 1}</h1>
-//           </div>
-//             <p style="padding-right: 0; width: 60%;">${name}</p>
-//             <div role="progressbars" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="--value:40; --level: 10"></div>
-//         </div>
-//       </div>`
-//       }
+        inner += `<div class="panel-content" id="item10">
+        <div class="item-card d-flex">
+          <div class="cross">
+            <h1>${i + 1}</h1>
+          </div>
+            <p style="padding-right: 0; width: 60%;">${name}</p>
+            <div role="progressbars" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="--value:40; --level: 10"></div>
+        </div>
+      </div>`
+      }
 
-//       document.getElementById("top-10").innerHTML = inner;
-//     }
-//   }
-// }
+      document.getElementById("top-10").innerHTML = inner;
+    }
+  }
+}
 
-// function LoadEKills() {
-//   const http = new XMLHttpRequest();
-//   const url = 'https://236f935f-8f97-4113-a394-ae28e91900a2.id.repl.co/TopE';
+function LoadEKills() {
+  const http = new XMLHttpRequest();
+  const url = 'https://236f935f-8f97-4113-a394-ae28e91900a2.id.repl.co/TopE';
 
-//   http.open("GET", url);
-//   http.send();
-//   http.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//       var rawJson = JSON.parse(http.responseText);
-//       var inner = "";
+  http.open("GET", url);
+  http.send();
+  http.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var rawJson = JSON.parse(http.responseText);
+      var inner = "";
 
-//       for (var i = 0; i < rawJson['L'].length; i++) {
-//         var object = rawJson['L'][i];
+      for (var i = 0; i < rawJson['L'].length; i++) {
+        var object = rawJson['L'][i];
 
-//         var name = object['name'];
-//         var kills = object['e'];
+        var name = object['name'];
+        var kills = object['e'];
 
-//         inner += `<div class="panel-content" id="item10">
-//         <div class="item-card d-flex">
-//           <div class="cross">
-//             <h1>${i + 1}</h1>
-//           </div>
-//             <p style="padding-right: 0; width: 60%;">${name}</p>
-//             <div role="progressbars" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="--value:40; --level: 10"></div>
-//         </div>
-//       </div>`
-//       }
+        inner += `<div class="panel-content" id="item10">
+        <div class="item-card d-flex">
+          <div class="cross">
+            <h1>${i + 1}</h1>
+          </div>
+            <p style="padding-right: 0; width: 60%;">${name}</p>
+            <div role="progressbars" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100" style="--value:40; --level: 10"></div>
+        </div>
+      </div>`
+      }
 
-//       document.getElementById("enemy-kills").innerHTML = inner;
-//     }
-//   }
-// }
+      document.getElementById("enemy-kills").innerHTML = inner;
+    }
+  }
+}
