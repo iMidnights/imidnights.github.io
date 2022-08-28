6b7fc4577ef2bcc1bd99a24383ad1f1fbcab2444
iMidnight <mohaidarus@gmail.com>
2022-08-28 14:50:31 +0100

    Removed

--- Oval-Knight/assets/script.js
+++
@@ -1,9 +0,0 @@
-function fscreen() {
-    var element = document.getElementById("embededGame");
-
-    if (element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-    } else if (element.webkitRequestFullScreen) {
-      element.webkitRequestFullScreen();
-    }
-}
